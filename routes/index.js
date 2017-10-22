f3b037e9626e92524f5fa1ca3fde61c49edc0ef5
hessyea <hasya101@gmail.com>
2017-10-22 18:19:05 +0200

    rerou sngup

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ module.exports = function(passport){
 	/* Handle Login POST */
 	router.post('/login', passport.authenticate('login', {
 		successRedirect: '/home',
-		failureRedirect: '/',
+		failureRedirect: '/signup',
 		failureFlash : true  
 	}));
 
