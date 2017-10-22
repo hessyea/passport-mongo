db562d67674d5596dad8e811bc77c4519d8aa373
hessyea <hasya101@gmail.com>
2017-10-22 17:30:01 +0200

    ]]s

--- passport/signup.js
+++ passport/signup.js
@@ -8,7 +8,7 @@ module.exports = function(passport){
             passReqToCallback : true // allows us to pass back the entire request to the callback
         },
         function(req, username, password, done) {
-		
+
             findOrCreateUser = function(){
                 // find a user in Mongo with provided username
                 User.findOne({ 'username' :  username }, function(err, user) {
@@ -23,19 +23,8 @@ module.exports = function(passport){
                         return done(null, false, req.flash('message','User Already Exists'));
                     } else {
                         // if there is no user with that email
-				
                         // create the user
-                        var newUser = new User();
-
-                        // set the user's local credentials
-                        newUser.username = username;
-                        newUser.password = createHash(password);
-                        newUser.email = req.param('email');
-                        newUser.firstName = req.param('firstName');
-                        newUser.lastName = req.param('lastName');
-
-
-			var doc = {      username: username
+                        var doc = {      username: username
                				, password: createHash(password)
                				, email: req.param('email')
                				, firstName: req.param('firstName')
@@ -54,14 +43,12 @@ module.exports = function(passport){
                     }
                 });
             };
-		
             // Delay the execution of findOrCreateUser and execute the method
             // in the next tick of the event loop
             process.nextTick(findOrCreateUser);
         })
     );
-	
-	
+
     // Generates hash using bCrypt
     var createHash = function(password){
         return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
