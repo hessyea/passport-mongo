06a41747e4d210f120bad0d773a3f00e1bfc3210
hessyea <hasya101@gmail.com>
2017-10-24 12:41:42 +0200

    geet REQ parms on Login

--- passport/login.js
+++ passport/login.js
@@ -9,18 +9,18 @@ module.exports = function(passport){
         },
         function(req, username, password, done) { 
             // check in mongo if a user with username exists or not
-            User.findOne({ 'username' :  username }, 
+            User.findOne({ 'username' :  req.param('username') }, 
                 function(err, user) {
                     // In case of any error, return using the done method
                     if (err)
                         return done(err);
                     // Username does not exist, log the error and redirect back
                     if (!user){
-                        console.log('User Not Found with username '+username);
+                        console.log('User Not Found with username '+req.param('username'));
                         return done(null, false, req.flash('message', 'User Not found.'));                 
                     }
                     // User exists but wrong password, log the error 
-                    if (!isValidPassword(user, password)){
+                    if (!isValidPassword(user, req.param('password'))){
                         console.log('Invalid Password');
                         return done(null, false, req.flash('message', 'Invalid Password')); // redirect back to login page
                     }
