4688ce3775edd30e91d701bf48512b88203944c2
hessyea <hasya101@gmail.com>
2017-12-02 15:59:09 +0100

    add dbgms

---
+++ routes/dbGmss.js
@@ -0,0 +1,18 @@
+
+var gambd = require('./models/gamdb');
+
+
+module.exports = function(req) {
+
+	var doc = {			username: req.body.gmName
+               				, password: req.body.password
+               			};
+				gambd.insert(doc, function (err, newDoc) {
+				if (err){
+                                	console.log('Error in Saving user: '+err);
+                                	throw err;
+				}
+				console.log('Game succesfully Cer8');
+				return done(null,newDoc);
+				})};
+
