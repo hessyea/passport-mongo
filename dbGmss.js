2420dba92bce692ad41d63c79a921fcae6ddad06
hessyea <hasya101@gmail.com>
2017-12-02 16:21:14 +0100

    nevv

--- dbGmss.js
+++ dbGmss.js
@@ -4,7 +4,6 @@ var gambd = require('../models/gamdb');
 
 
 module.exports = function(req) {
-
 	var doc = {			username: req.body.gmName
                				, password: req.body.password
                			};
