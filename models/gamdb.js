c3f998ad5b417d5600f83d9e1f22ac6c7ed4cdc8
hessyea <hasya101@gmail.com>
2017-12-02 13:23:29 +0100

    gemsess0001

---
+++ models/gamdb.js
@@ -0,0 +1,8 @@
+var Datastore = require('nedb');
+var db = new Datastore({
+  filename: 'gamedb.db', // Provide a path to the database file.
+  autoload: true, // Automatically load the database.
+  timestampData: true // Add and manage the fields createdAt and updatedAt.
+});
+
+module.exports = db;
