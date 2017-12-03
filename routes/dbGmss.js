4de0fd39748e19ca3f978b93594f89ab757dc2df
hessyea <hasya101@gmail.com>
2017-12-03 11:48:39 +0100

    return gambd.insert

--- routes/dbGmss.js
+++ routes/dbGmss.js
@@ -7,14 +7,14 @@ module.exports = function(req) {
 	var doc = {			gnme: req.body.gmName
                				, password: req.body.password
                			};
-				gambd.insert(doc, function (err, newDoc) {
+				return gambd.insert(doc, function (err, newDoc) {
 				if (err){
                                 	console.log('Error in Saving user: '+err);
                                 	throw err;
 				}
 				
 				console.log('Game succesfully Cer8' + req.body.gmName);
-				return newDoc;
+
 				})
 					//gambd.find(req.body.gmName,function (err, docs) {
 					
