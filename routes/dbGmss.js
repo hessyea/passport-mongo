d9e730f5b40a8874aa10990ca0a314458605cbb5
hessyea <hasya101@gmail.com>
2017-12-03 13:04:08 +0100

    rezturn return

--- routes/dbGmss.js
+++ routes/dbGmss.js
@@ -4,17 +4,18 @@ var evilGlob
 
 module.exports = function(req) {
 
-	var doc = {			gnme: req.body.gmName
+				var doc = {			
+					gnme: req.body.gmName
                				, password: req.body.password
                			};
-				gambd.insert(doc, function (err, newDoc) {
+				gambd.insert(doc,evilGlob = function (err, newDoc) {
 				if (err){
                                 	console.log('Error in Saving game: '+err);
                                 	throw err;
 				}
 				
 				console.log('Game succesfully Cer8' + req.body.gmName);
-					evilGlob= newDoc;
+					return newDoc;
 
 				})
 					//gambd.find(req.body.gmName,function (err, docs) {
