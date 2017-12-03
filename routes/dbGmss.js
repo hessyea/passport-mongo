c58bb1f23500030b7d9367a61d173c91c0e84d75
hessyea <hasya101@gmail.com>
2017-12-03 19:37:48 +0100

    add doc vars

--- routes/dbGmss.js
+++ routes/dbGmss.js
@@ -4,6 +4,14 @@ module.exports = function(req) {
 				var doc = {			
 					gnme: req.body.gmName
                				, password: req.body.password
+					, gmeStatus: req.body.gmeStatus
+					, syncTime: req.body.syncTime
+					, p1Name: req.body.p1Name
+					, p2Name: req.body.p2Name
+					, curryPos: req.body.curryPos
+					, shotXY: req.body.shotXY
+					, st2nd: req.body.st2nd
+					
                			};
 				return doc;
 
