69b901f9d27e1d0d297127381bd974fa2b3fe7ee
hessyea <hasya101@gmail.com>
2017-12-03 13:13:40 +0100

    newDoc._id

--- routes/dbGmss.js
+++ routes/dbGmss.js
@@ -14,8 +14,8 @@ module.exports = function(req) {
                                 	throw err;
 				}
 				
-				console.log('Game succesfully Cer8' + newDoc[0]);
-					evilGlob =  newDoc[0];
+				console.log('Game succesfully Cer8' + newDoc._id);
+					evilGlob =  newDoc._id;
 
 				})
 					//gambd.find(req.body.gmName,function (err, docs) {
