97743b9a4559a3747b4a73576a427b6a03a091b0
hessyea <hasya101@gmail.com>
2017-12-08 20:06:44 +0100

    braky

--- routes/index.js
+++ routes/index.js
@@ -128,7 +128,7 @@ module.exports = function(passport){
 			//console.log(docsMyGame._id+"-------");
 			if(docsMyGame == null){
 
-				gamdb33.find({{lookingFP: { $exists: true }}, { lookingFP: 'true' }, {$not: { _id: req.body._id }}}).sort({ createdAt: -1 }).exec(function (err, docsOther) {
+				gamdb33.find({lookingFP: { $exists: true }, lookingFP: 'true' , $not: { _id: req.body._id }}).sort({ createdAt: -1 }).exec(function (err, docsOther) {
 					console.log(docsOther+"-------");
 					if(docsOther != null && docsOther._id != req.body._id){
 						gamdb33.findOne({_id: req.body._id}, function (err, docsSelf) {
