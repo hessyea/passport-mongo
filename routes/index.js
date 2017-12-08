976aa9552c5cbc9cc2630e4ea097f1b7beabcb25
hessyea <hasya101@gmail.com>
2017-12-08 20:59:28 +0100

    gg

--- routes/index.js
+++ routes/index.js
@@ -130,7 +130,7 @@ module.exports = function(passport){
 				console.log("notNull-------");
 				gamdb33.find({lookingFP: { $exists: true }, lookingFP: 'true' , $not: { _id: req.body._id }}).sort({ createdAt: -1 }).exec(function (err, docsOther) {
 					console.log(docsOther+"-------");
-					if(docsOther != null){
+					if(docsOther[0]){
 						console.log(docsOther+"---!= null");
 						gamdb33.findOne({_id: req.body._id}, function (err, docsSelf) {
 							gamdb33.insert(createMtchSC(docsSelf,docsOther),function (err, newDoc) {
