ccdfcf3476c6f527abf72b3c6c794b5ae86c660c
hessyea <hasya101@gmail.com>
2017-12-14 19:26:46 +0100

    hh

--- routes/index.js
+++ routes/index.js
@@ -129,7 +129,7 @@ module.exports = function(passport){
 	gamdb33.update({ _id: req.body.gID },setVal(req.body.pmNR,req.body.maP),
 			{ multi: false },
 			function (err, numReplaced) {
-				console.log(err+" nr "+numReplaced+"iiiii"+req.body.gID+)
+				console.log(err+" nr "+numReplaced+"iiiii"+req.body.gID)
 					res.send("ok");
 			});
 	});
