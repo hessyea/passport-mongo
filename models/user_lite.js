ef3fe11c26189f6c3b4367141475652cbf495bb6
hessyea <hasya101@gmail.com>
2017-12-20 22:59:16 +0100

    gv

--- models/user_lite.js
+++ models/user_lite.js
@@ -1,6 +1,8 @@
 var db = require('mongo-lite').connect('mongodb://hexxua:67u15po@ds161346.mlab.com:61346/mgcs101', ['users']);
+
+
+db.users = db.collection('users')
 db.users.insert({title: 'first'}, function(err, doc){})
 var exp = db.users
-console.log(db.getCollectionNames())
 
 module.exports = exp;
