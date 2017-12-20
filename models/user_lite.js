138ab1264d1410b49e1722f4ad202fc2aa3dcc1c
hessyea <hasya101@gmail.com>
2017-12-20 22:30:59 +0100

    ex

--- models/user_lite.js
+++ models/user_lite.js
@@ -1,4 +1,4 @@
 var db = require('mongo-lite').connect('mongodb://hexia:678u15po@ds161346.mlab.com:61346/mgcs101', ['users', 'comments']);
-var db = db.users
+var exp = db.users = db.collection('users')
 
-module.exports = db;
+module.exports = exp;
