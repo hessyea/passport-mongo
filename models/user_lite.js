17e4095fab21127c904c9634e9bd0d9a5b5a580a
hessyea <hasya101@gmail.com>
2017-12-20 22:54:41 +0100

    cs

--- models/user_lite.js
+++ models/user_lite.js
@@ -1,5 +1,6 @@
 var db = require('mongo-lite').connect('mongodb://hexxua:67u15po@ds161346.mlab.com:61346/mgcs101', ['users']);
 db.users.insert({title: 'first'}, function(err, doc){})
 var exp = db.users
+console.log(db.getCollectionNames())
 
 module.exports = exp;
