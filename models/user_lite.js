e2e2f104ba49c7a2b4bfee4aa42c75630aeee500
hessyea <hasya101@gmail.com>
2017-12-21 16:22:02 +0100

    -

--- models/user_lite.js
+++ models/user_lite.js
@@ -1,7 +1,7 @@
 var db = require('mongo-lite').connect(process.env.PROD_MONGODB);
 
-db.users22 = db.collection('users')
-db.users22.insert({title: 'first'}, function(err, doc){})
-var exp = db.users22
+db.users22 = db.collection('users');
+db.users22.insert({title: 'first'}, function(err, doc){});
+var exp = db.users22;
 
 module.exports = exp;
