718cc78fe3cebfeaf06d971b3a8eefba89852ecd
hessyea <hasya101@gmail.com>
2017-12-21 16:32:10 +0100

    }

--- models/user_lite.js
+++ models/user_lite.js
@@ -1,4 +1,4 @@
-var db = require('mongo-lite').connect(process.env.PROD_MONGODB);
+var db = require('mongo-lite').connect(process.env.PROD_MONGODB,['posts']);
 
 db.users22 = db.collection('users');
 db.users22.insert({title: 'first'}, function(err, doc){});
