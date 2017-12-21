43226530b2348dd1d79a0cc559d35cd3417fc8c5
hessyea <hasya101@gmail.com>
2017-12-21 16:14:07 +0100

    vaars

--- models/user_lite.js
+++ models/user_lite.js
@@ -1,5 +1,4 @@
-var db = require('mongo-lite').connect('mongodb://hexxua:67u15po@ds161346.mlab.com:61346/mgcs101');
-
+var db = require('mongo-lite').connect(process.env.PROD_MONGODB);
 
 db.users22 = db.collection('users')
 db.users22.insert({title: 'first'}, function(err, doc){})
